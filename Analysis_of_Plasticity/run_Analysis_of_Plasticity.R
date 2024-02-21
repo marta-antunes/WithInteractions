395d6895f092e4c19f141abeb7d3e344c6d74caf
marta-antunes <marta-antunes@users.noreply.github.com>
2024-02-21 10:07:43 +0000

    Update run_Analysis_of_Plasticity.R

--- Analysis_of_Plasticity/run_Analysis_of_Plasticity.R
+++ Analysis_of_Plasticity/run_Analysis_of_Plasticity.R
@@ -1,9 +1,4 @@
-# G23 dataset
-#setwd("/home/mrtntns/Documents/temp_julho2022/git/DiffExpWithInteractions/General_withoutF")
-#install lme4
-#install.packages("lme4",
-#                 repos=c("http://lme4.r-forge.r-project.org/repos",
-#                         getOption("repos")[["CRAN"]]))
+# Analysis of Plasticity 
 
 #necessary
 library(lme4)
@@ -11,16 +6,11 @@ library(car)
 library(glmmTMB)
 library(bbmle) #necessary to AIC
 
-
-#args <- commandArgs(TRUE)
 ##tive de remover o espaço no início do header
-#normalizedCounts <- read.table(args[1], sep = '\t', header=TRUE, stringsAsFactors = TRUE)
 normalizedCounts <- read.table("Galaxy108-Normalized_counts.tabular", sep = '\t', header=TRUE, stringsAsFactors = TRUE)
-
 
 #keep genes that have data in at least 3 of the samples (columns).
 dataInAtLeastXsamples <- normalizedCounts[ rowSums( normalizedCounts > 0 ) >= 3, ]
-
 
 #transpose
 transposed_of_normalizedCounts <- t(dataInAtLeastXsamples)    #transpose the rows and columns
@@ -28,13 +18,10 @@ transposed_of_normalizedCounts <- t(dataInAtLeastXsamples)    #transpose the row
 #read samples
 rownames <- rownames(transposed_of_normalizedCounts)
 splited <- strsplit(rownames, split = "_")
-#History<-substr(lapply(splited, `[[`, 2), start = 1, stop = 2) #list of lists. history is nested in the second element of list  #lapply gives the second element of each list #substr takes the first two characters of each string
 Environment<-lapply(splited, `[[`, 4) #Environment is the fourth element of list
 Environment<-as.character(Environment) #transform list in character
 AP <-lapply(splited, `[[`, 2)  #AP is the second element of list
 AP<-as.character(AP) #transform list in character
-#Selection <-lapply(splited, `[[`, 1) #selection is the first element of list
-#Selection<-as.character(Selection) #transform list in character
 
 #Creating new data frame - name: NewColumns.Df,   
 #Contains four columns History, Environment, AP and Selection
@@ -52,20 +39,6 @@ for(i in 1:ncol(data1)) {       # for-loop over columns
   meanPTW <- (data3[4,1]+data3[5,1]+data3[6,1])/3    #PT_TREATMENT_counts are in lines 4,5 and 6 of the first column
   meanPTC <- (data3[1,1]+data3[2,1]+data3[3,1])/3       #PT_CONTROL_counts are in lines 1,2 and 3 of the first column
   upOrDownPT<-meanPTW-meanPTC
-  #print(paste0("PT Up or Down? ", upOrDownPT))
-  #meanNLW <- (data3[5,1]+data3[7,1]+data3[9,1])/3    #NL_TREATMENT_counts are in lines 5,7 and 9 of the first column
-  #meanNLC <- (data3[4,1]+data3[6,1]+data3[8,1])/3    #NL_CONTROL_counts are in lines 4,6 and 8 of the first column
-  #upOrDownNL<-meanNLW-meanNLC
-  #print(paste0("NL Up or Down?", upOrDownNL))
-  # if you have a third factor...
-  #meanWPTW <- (data3[7,1]+data3[8,1]+data3[9,1])/3    #WPT_TREATMENT_counts are in lines 7,8 and 9 of the first column
-  #meanWPTC <- (data3[4,1]+data3[5,1]+data3[6,1])/3       #WPT_CONTROL_counts are in lines 4,5 and 6 of the first column
-  #upOrDownWPT<-meanWPTW-meanWPTC
-  #print(paste0("WPT Up or Down? ", upOrDownWPT))
-  #meanWNLW <- (data3[20,1]+data3[22,1]+data3[24,1])/3    #WNL_TREATMENT_counts are in lines 20,22 and 24 of the first column
-  #meanWNLC <- (data3[19,1]+data3[21,1]+data3[23,1])/3    #WNL_CONTROL_counts are in lines 19,21 and 23 of the first column
-  #upOrDownWNL<-meanWNLW-meanWNLC
-  #print(paste0("WNL Up or Down? ", upOrDownWNL))
   Expression <- data3[,1]   #expression is always in the first column, this way we can vary the number of factors without needing to alter this line of code
   options(contrasts=c("contr.sum","contr.poly"))
   glmmTMB1_binN = try(glmmTMB(Expression ~ Environment+(1|AP),data=data3,family="nbinom2"))
@@ -94,6 +67,5 @@ for(i in 1:ncol(data1)) {       # for-loop over columns
   #transpose
   transposed_of_AnovaGlmmTMB1_binN <- as.data.frame(t(subsetted),stringsAsFactors = FALSE)
   try(write.table(transposed_of_AnovaGlmmTMB1_binN, "PT_onlyPlasticity.csv" , append = TRUE, sep = '\t', col.names = FALSE, row.names = TRUE))
-  #try(write.table(transposed_of_AnovaGlmmTMB1_binN, args[2] , append = TRUE, sep = '\t', col.names = FALSE, row.names = TRUE))
 
 }
