aac029216857ed5c18fa81501747c61f1c5cd276
marta-antunes <marta-antunes@users.noreply.github.com>
2024-04-05 13:55:02 +0100

    incorporation of ancestry

--- normalizedCounts2PCA.R
+++ normalizedCounts2PCA.R
@@ -1,6 +1,5 @@
 # G23 dataset
 
-
 #necessary
 library(lme4)
 library(car)
@@ -26,9 +25,10 @@ project.pca.proportionvariances <- ((project.pca$sdev^2) / (sum(project.pca$sdev
 par(mar=c(4,4,1,1), mfrow=c(1,1), cex=1.0, cex.main=0.8, cex.axis=0.8)
 
 #Plots scatter plot for PC 1 and 2
+#high latitude populations represented by empty center
 plot(project.pca$x, type="n", xlab=paste("PC1, ", round(project.pca.proportionvariances[1], 2), "%"), ylab=paste("PC2, ", round(project.pca.proportionvariances[2], 2), "%"))
-points(project.pca$x, col=c('blue','blue','blue','blue','red','blue','red','blue','red','red','red','red','blue','blue','blue','red','red','red','blue','red','blue','red','blue','red'), pch=c(16,16,16,16,16,16,16,16,16,16,16,16,15,15,15,15,15,15,15,15,15,15,15,15), cex=1,)
+points(project.pca$x, col=c('blue','blue','blue','blue','red','blue','red','blue','red','red','red','red','blue','blue','blue','red','red','red','blue','red','blue','red','blue','red'), pch=c(16,16,16,21,21,21,21,21,21,16,16,16,15,15,15,15,15,15,22,22,22,22,22,22), cex=1)
 #initially we included the name of the samples but them we realize we dont need, if you want to rescue that uncoment the line below
 #text(project.pca$x, labels=rownames(project.pca$x), cex=0.6, font=2, pos=2)
-legend("topright",legend=c("Warming environment", "Control environment", "Control Regime", "Warming Regime"),col=c("red", "blue","grey38", "grey38"), lty = c(0, 0, 0), cex=0.8, pch = c(16, 16, 16, 15))
+legend("topright",legend=c("Warming environment", "Control environment", "Control Regime", "Warming Regime", "Low latitude", "High latitude"),col=c("red", "blue","grey38", "grey38", "grey38", "grey38"), lty = c(0, 0, 0), cex=0.8, pch = c(16, 16, 16, 15, 16, 21))
 
